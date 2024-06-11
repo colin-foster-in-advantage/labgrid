884184711222323dabad2e395c3f49026f32e360
Colin Foster <colin.foster@in-advantage.com>
2024-06-10 20:57:08 -0500

    driver: power: tplink: add support for smart plugs
    TPLink sells single plugs that don't follow the parent / children
    pattern. Add support for these devices.
    
    Signed-off-by: Colin Foster <colin.foster@in-advantage.com>

--- labgrid/driver/power/tplink.py
+++ labgrid/driver/power/tplink.py
@@ -1,22 +1,39 @@
 """ Tested with TP Link KP303, and should be compatible with any strip supported by kasa """
 
 import asyncio
-from kasa import SmartStrip
+from kasa import SmartStrip, SmartPlug, SmartDevice
+
+
+def _target_get(device: SmartDevice, index):
+    if device.is_strip:
+        assert (
+            len(device.children) > index
+        ), "Trying to access non-existant plug socket on strip"
+        return device.children[index]
+    elif device.is_plug:
+        return device
+    assert(), "Trying to access invalid device"
+
+
+def _device_get(host, index):
+    if index < 0:
+        device = SmartPlug(host)
+    else:
+        device = SmartStrip(host)
+    return device
 
 
 async def _power_set(host, port, index, value):
     """We embed the coroutines in an `async` function to minimise calls to `asyncio.run`"""
     assert port is None
     index = int(index)
-    strip = SmartStrip(host)
-    await strip.update()
-    assert (
-        len(strip.children) > index
-    ), "Trying to access non-existant plug socket on strip"
+    device = _device_get(host, index)
+    await device.update()
+    target = _target_get(device, index)
     if value is True:
-        await strip.children[index].turn_on()
+        await target.turn_on()
     elif value is False:
-        await strip.children[index].turn_off()
+        await target.turn_off()
 
 
 def power_set(host, port, index, value):
@@ -26,9 +43,7 @@ def power_set(host, port, index, value):
 def power_get(host, port, index):
     assert port is None
     index = int(index)
-    strip = SmartStrip(host)
-    asyncio.run(strip.update())
-    assert (
-        len(strip.children) > index
-    ), "Trying to access non-existant plug socket on strip"
-    return strip.children[index].is_on
+    device = _device_get(host, index)
+    asyncio.run(device.update())
+    target = _target_get(device, index)
+    return target.is_on
